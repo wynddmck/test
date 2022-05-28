ee1fce5611ab17549c11938eb556bf10d11ad749
wynddmck <m15087023762@163.com>
2022-05-28 10:21:19 +0800

    提交代码

---
+++ Solution1/quickdate/源.c
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//int main()
+//{
+//	char arr1[] = "abc\x48";
+//	char arr2[] = { 'a','b','c'};
+//	int am;
+//	am = strlen(arr1);
+//	printf("%d\n",am);
+//	printf("%s\n",arr1);
+//	return 0;
+//}
+//int main()
+//{
+//	int input=0;
+//	printf("你要好好学习吗？(1/0)\n");
+//	scanf("%d",&input);
+//	if (input == 1)
+//		printf("给你一个好offer\n");
+//	else if (input == 0)
+//		printf("烂泥扶不上墙\n");
+//	else if (input == 2)
+//		printf("走点儿心呀\n");
+//	else
+//		printf("咋的，回家啃老呀\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int line = 0;
+//	while (line <= 3)
+//	{
+//		printf("敲代码\n");
+//		line=line + 1;
+//	}
+//	printf("大牛\n");
+//	return 0;
+//}
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int first=0;
+//	int second = 0;
+//	int sum = 0;
+//	printf("请输入两个数字：\n");
+//	scanf("%d%d",&first,&second);
+//	sum = Add(first,second);
+//	printf("%d", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		printf("%d\n",arr[i]);
+//		i++;
+//	}
+//	return 0;
+//}
+int main()
+{
+	char test = 0;
+	int test1 = 5;
+	printf("%d",test1);
+	return 0;
+}
